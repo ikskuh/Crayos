5033527663184620d621f9d925fb6c28400d0363
Felix "xq" Queißner <git@random-projects.net>
2024-01-26 23:13:02 +0100

    Smaller backend refactoring.

--- backend/game/session.go
+++ backend/game/session.go
@@ -73,58 +73,61 @@ func (session *Session) Destroy() {
 func (session *Session) AddPlayer(new *Player) {
 
 	log.Println("Player joins", new)
-	if session.Flags&SESSION_JOINABLE != 0 {
-
-		session.Players[new] = true
-
-		new.SendChan <- &EnterSessionEvent{
-			SessionId: session.Id,
-		}
-
-		{
-			nicknames := make([]string, len(session.Players))
-
-			i := 0
-			for k := range session.Players {
-				nicknames[i] = k.NickName
-				i++
-			}
-
-			for player := range session.Players {
-				player.SendChan <- &PlayersChangedEvent{
-					Players: nicknames,
-					Joined:  &new.NickName,
-				}
-			}
-		}
-
-		new.SendChan <- &ChangeGameViewEvent{
-			ViewID: GAMEVIEW_LOBBY,
-		}
-
-	} else {
+	if session.Flags&SESSION_JOINABLE == 0 {
 		new.SendChan <- &JoinSessionFailedEvent{
 			Reason: "Session is already running.",
 		}
+		return
+	}
+
+	session.Players[new] = true
+
+	new.SendChan <- &EnterSessionEvent{
+		SessionId: session.Id,
+	}
+
+	{
+		nicknames := make([]string, len(session.Players))
+
+		i := 0
+		for k := range session.Players {
+			nicknames[i] = k.NickName
+			i++
+		}
+
+		session.Broadcast(&PlayersChangedEvent{
+			Players: nicknames,
+			Joined:  &new.NickName,
+		})
+	}
+
+	new.SendChan <- &ChangeGameViewEvent{
+		ViewID: GAMEVIEW_LOBBY,
+	}
+
+}
+
+func (session *Session) Broadcast(msg Message) {
+	for player := range session.Players {
+		player.SendChan <- msg
 	}
 }
 
-func (session *Session) Run() {
-	log.Println("Starting ", session.Id, " opened")
-	defer log.Println("Session ", session.Id, " closed")
+func (session *Session) PumpEvents() *PlayerMessage {
 	for len(session.Players) > 0 {
-
 		select {
-		case pmsg, ok := <-session.InboundDataChan:
-
-			log.Println("inbound message from player", pmsg, ok)
+		case pmsg := <-session.InboundDataChan:
+			return &pmsg
 
 		case new := <-session.JoinChan:
 			session.AddPlayer(new)
 
 		case old := <-session.LeaveChan:
-			log.Println("Player leaves", old)
 
+			// TODO(fqu): Check if player is active player in the session,
+			// so we can skip
+
+			log.Println("Player leaves", old)
 			delete(session.Players, old)
 
 			// case <- ticker.C:
@@ -133,6 +136,20 @@ func (session *Session) Run() {
 			// 		return
 			// 	}
 		}
+	}
+	return nil
+}
+
+func (session *Session) Run() {
+	log.Println("Starting ", session.Id, " opened")
+	defer log.Println("Session ", session.Id, " closed")
+	for len(session.Players) > 0 {
+
+		pmsg := session.PumpEvents()
+		if pmsg == nil {
+			return
+		}
+
 	}
 
 }
