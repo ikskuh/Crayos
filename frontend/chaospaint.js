ab67976a4ed5590eb76d800da92428246ce39939
Fabio Arnold <fabio@fabioarnold.de>
2024-01-26 22:52:15 +0100

    Use background

--- frontend/chaospaint.js
+++ frontend/chaospaint.js
@@ -4,6 +4,8 @@ let paletteElem;
 // CONSTANTS
 const width = 1920;
 const height = 1080;
+const lineWidth = 20;
+const distanceThreshold = 10; // minimum distance between points to add a new point
 
 const palette = [
   "#FFF",
@@ -17,17 +19,47 @@ const palette = [
 ];
 let selectedColor = 7;
 
+const paths = [];
+
+const backgroundUrls = [
+  "img/graveyard.png",
+];
+const backgrounds = [];
+
+function distanceSquared(p1, p2) {
+  return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
+}
+
+function loadBackgrounds() {
+  for (let i = 0; i < backgroundUrls.length; i++) {
+    const img = new Image();
+    img.src = backgroundUrls[i];
+    img.onload = () => {
+      drawCanvas();
+    }
+    backgrounds.push(img);
+  }
+}
+
 function initPainter() {
   canvas = document.getElementById("canvas");
-  const ctx = canvas.getContext("2d");
+
+  loadBackgrounds();
+
+  canvas.onmousedown = (e) => {
+    paths.push({ color: palette[selectedColor], points: [{ x: e.offsetX, y: e.offsetY }] });
+  };
 
   canvas.onmousemove = (e) => {
     console.log(e.button);
     if (e.buttons & 1) {
-      ctx.beginPath();
-      ctx.rect(e.offsetX, e.offsetY, 20, 20);
-      ctx.fillStyle = palette[selectedColor];
-      ctx.fill();
+      const currentPath = paths[paths.length - 1];
+      const lastPoint = currentPath.points[currentPath.points.length - 1];
+      const newPoint = { x: e.offsetX, y: e.offsetY };
+      if (distanceSquared(lastPoint, newPoint) > distanceThreshold * distanceThreshold) {
+        currentPath.points.push(newPoint);
+      }
+      drawCanvas();
     }
   };
 
@@ -40,6 +72,23 @@ function initPainter() {
   window.addEventListener("resize", resize);
 
   initPalette();
+}
+
+function drawCanvas() {
+  const ctx = canvas.getContext("2d");
+  // ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(backgrounds[0], 0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < paths.length; i++) {
+    const path = paths[i];
+    ctx.beginPath();
+    ctx.moveTo(path.points[0].x, path.points[0].y);
+    for (let j = 1; j < path.points.length; j++) {
+      ctx.lineTo(path.points[j].x, path.points[j].y);
+    }
+    ctx.strokeStyle = path.color;
+    ctx.lineWidth = lineWidth;
+    ctx.stroke();
+  }
 }
 
 const colorw = 122;
