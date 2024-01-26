aaab002c90fc10e8d55457759b5078b1b75b399f
Fabio Arnold <fabio@fabioarnold.de>
2024-01-26 20:41:46 +0100

    Add mockups

--- frontend/index.js
+++ frontend/index.js
@@ -2,12 +2,19 @@ let canvas;
 let ctx;
 
 // CONSTANTS
-const width = 1600;
-const height = 900;
+const width = 1920;
+const height = 1080;
 
 function init() {
   canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d");
+
+  canvas.onmousemove = (e) => {
+    ctx.beginPath();
+    ctx.rect(e.offsetX, e.offsetY, 20, 20);
+    ctx.fillStyle = 'black;'
+    ctx.fill();
+  };
 
   const resize = (event) => {
     const wrapper = document.getElementById("wrapper");
