ac425fd1412c52d0436f0fbb39988df9bbaf68a6
Manello <Manuel@Baeuerle.email>
2024-01-27 11:21:28 +0100

    adding host events

--- frontend/index.js
+++ frontend/index.js
@@ -4,7 +4,11 @@ const Open = 1;
 let socket;
 let sessionID = NoSession;
 
-let ServerSideDisconnect = false;
+let serverSideDisconnect = false;
+
+let nicknames = ["nickname", "", "", ""];
+const gamestates = ["title", "connecting", "connection_failed", "lobby", "painer", "viewer", "troll", "voting", "winner"];
+let currentGamestate = "connecting";
 
 function init() {
     initSocket();
@@ -13,22 +17,18 @@ function init() {
 
 function initSocket() {
     document.getElementById("connecting").style.display = "flow";
-    socket = new WebSocket("ws://192.168.0.100:8090");
+    socket = new WebSocket("ws://192.168.37.247:8090/ws");
 
-    socket.onopen = (event) => {
-        initTitle();
-    };
-    socket.addEventListener("error", (event) => {
-        console.log("WebSocket error: ", event);
-    });
+    socket.onerror = function(event){console.log("WebSocket error: ", event);}
+    socket.onopen = function(event){setView("title")};
+    socket.onmessage = function(event){onSocketReceive(event)};
 
     setInterval(timeoutCheck, 3000);
 }
 
 function timeoutCheck() {
-    if (socket.readyState != Open & ServerSideDisconnect == false) {
-        document.getElementById("connection_failed").style.display = "flow";
-        hideSection("connecting");
+    if (socket.readyState != Open && serverSideDisconnect == false) {
+        setView("connection_failed");
     }
 }
 
@@ -42,4 +42,29 @@ function hideSection(id) {
 
 function showSection(id) {
     document.getElementById(id).style.display = "flow";
+}
+
+function setView(newState) {
+    hideSection(currentGamestate);
+    currentGamestate = newState;
+    showSection(newState);
+}
+
+function onSocketReceive(event) {
+    let data = JSON.parse(event.data);
+    console.log(data);
+
+    switch (data.type) {
+        case "change-game-view-event":
+            setView(data.view);
+            break;
+        case "players-changed-event":
+            
+            break;
+        case "enter-session-event":
+            sessionID = data.session;
+            break;
+        
+
+    }
 }
--- frontend/title.js
+++ frontend/title.js
@@ -1,13 +1,9 @@
-function initTitle() {
-    document.getElementById("title").style.display = "flow";
-}
-
-function exitTitle() {
-
-}
-
 function createGame() {
-
+    nicknames[0] = document.getElementById("nicknameInput").value;
+    socket.send(JSON.stringify({
+        type: "create-session-command",
+        nickname: nicknames[0]
+    }));
 }
 
 function joinGame() {
