52dd5c36d454aff7c9278c69c6230b77e6fa11b2
PhilippWendel <41269321+PhilippWendel@users.noreply.github.com>
2024-01-27 15:56:42 +0100

    Added sending player state

--- backend/game/session.go
+++ backend/game/session.go
@@ -187,6 +187,17 @@ func (session *Session) PumpEvents(timeout chan time.Time) *PlayerMessage {
 	return nil
 }
 
+func broadcastPlayerReadyState(s *Session, m map[*Player]bool) {
+	log.Println("Sending PlayerReadyState")
+	readyMap := make(map[string]bool)
+	for p, b := range m {
+		readyMap[p.NickName] = b
+	}
+	s.Broadcast(&PlayerReadyChangedEvent{
+		Players: readyMap,
+	})
+}
+
 func (session *Session) Run() {
 	log.Println("Starting ", session.Id, " opened")
 	defer log.Println("Session ", session.Id, " closed")
@@ -196,8 +207,11 @@ func (session *Session) Run() {
 	for len(session.Players) > 0 {
 
 		// show lobby
-		var startGame = false
-		for startGame {
+		// startGame := false
+		// playersReady := false
+		playersReadyMap := make(map[*Player]bool)
+		// len(session.Players) < 2 && !playersReady && !startGame
+		for true {
 			pmsg := session.PumpEvents(no_timeout)
 			if pmsg == nil {
 				return
@@ -205,7 +219,13 @@ func (session *Session) Run() {
 
 			switch msg := pmsg.Message.(type) {
 			case *UserCommand:
-				startGame = (msg.Action == USER_ACTION_SET_READY) && len(session.Players) >= 2
+				switch msg.Action {
+				case USER_ACTION_SET_READY:
+					playersReadyMap[pmsg.Player] = true
+				case USER_ACTION_SET_NOT_READY:
+					playersReadyMap[pmsg.Player] = false
+				}
+				broadcastPlayerReadyState(session, playersReadyMap)
 			}
 		}
 
